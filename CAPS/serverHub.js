6cdd7b655373040eda8bc12773f0110a5c99e2c7
mohammadalmoqdad <almekad.mohammad@gmail.com>
2021-01-19 14:58:42 +0200

    fix lab without tests

--- CAPS/serverHub.js
+++ CAPS/serverHub.js
@@ -2,7 +2,6 @@
 
 const net = require('net');
 const port = process.env.PORT || 3000;
- const uuID = require('uuid');
 const server = net.createServer();
 server.listen(port, ()=> console.log(`server is running on ${port}`));
 
@@ -10,14 +9,11 @@ let socketPool = {}; // define it outside f the connection just to avoid remove 
 
 server.on('connection', (socket)=> {
     //Accept inbound TCP connections on a declared port
-
     console.log("SERVER GOT A CONNECTION!");
-    // console.log(socket); this like the object of response and request!!     
 
     // giving each client a unique ID
     const id = `Socket-${Math.random()}`;
     socketPool[id] = socket;
-    console.log("/////////////////// uuid: ",uuID.NIL);
    
     socket.on('data', (buffer)=> dispatchEvent(buffer));   //Read and parse the incoming data/payload
     socket.on('error', (e)=> console.log('SOCKET ERROR', e));
@@ -27,9 +23,9 @@ server.on('connection', (socket)=> {
 
 function dispatchEvent(buffer) {
     
-    console.log("buffer >>>>>> ", buffer);
+  
     let msg = JSON.parse(buffer.toString().trim()); //format msg
-    console.log("msg >>>> ", msg);
+    console.log("Event : ", msg);
 
      broadcast(msg); //send to all clients
 
