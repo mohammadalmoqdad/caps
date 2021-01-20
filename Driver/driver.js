52ae5549603e891c92e357ca0f99fd94981c1506
mohammadalmoqdad <almekad.mohammad@gmail.com>
2021-01-20 03:22:17 +0200

    finish lab without tests

--- Driver/driver.js
+++ Driver/driver.js
@@ -1,43 +1,93 @@
+'use strict';
+
 require('dotenv').config();
 
 
-const net = require('net');
-const client = new net.Socket();
+const io = require('socket.io-client');
+const caps = io.connect(`http://localhost:3000/caps`);
 
-const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 3000;
+caps.on('pickup', (payload) => {
+  setTimeout(() => {
+    console.log(`picked up ${payload.orderId}`);
+    caps.emit('in-transit', payload);
+  }, 1500);
 
-
-client.connect(port, host, () => {
-    console.log("DRIVER CONNECT MSG");
-})
-
-
-
-    client.on('data', (data) => {
-        let msg = JSON.parse(data); 
-        if (msg.event == "pickup") {
-            setTimeout(function () {
-                console.log(`DRIVER: picked up ${msg.payload.orderId}`)
-               let newObj = JSON.stringify({
-                   event: "in-transit",
-                   payload : msg.payload
-               }) 
-               client.write(newObj);
-
-            }, 1000);
-
-            setTimeout(function () {
-               let newObj = JSON.stringify({
-                   event: "delivered",
-                   payload : msg.payload
-               }) 
-               client.write(newObj);
-
-            }, 3000);
+  setTimeout(() => {
+    console.log(`delivered up ${payload.orderId}`);
+    caps.emit('delivered', payload);
+  }, 3000);
+});
 
 
 
-        }
 
-    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const net = require('net');
+// const client = new net.Socket();
+
+// const host = process.env.HOST || 'localhost';
+// const port = process.env.PORT || 3000;
+
+
+// client.connect(port, host, () => {
+//     console.log("DRIVER CONNECT MSG");
+// })
+
+
+
+//     client.on('data', (data) => {
+//         let msg = JSON.parse(data); 
+//         if (msg.event == "pickup") {
+//             setTimeout(function () {
+//                 console.log(`DRIVER: picked up ${msg.payload.orderId}`)
+//                let newObj = JSON.stringify({
+//                    event: "in-transit",
+//                    payload : msg.payload
+//                }) 
+//                client.write(newObj);
+
+//             }, 1000);
+
+//             setTimeout(function () {
+//                let newObj = JSON.stringify({
+//                    event: "delivered",
+//                    payload : msg.payload
+//                }) 
+//                client.write(newObj);
+
+//             }, 3000);
+
+
+
+//         }
+
+//     })
